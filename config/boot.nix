3dce3b0f47878805f3a8949627bf31c592b63c1f
quentin@aristote.fr <quentin@aristote.fr>
2024-07-27 18:16:36 +0200

    boot: kernel: disable xanmod patches

--- config/boot.nix
+++ config/boot.nix
@@ -1,11 +1,7 @@
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   personal.boot = {
     grub.enable = true;
   };
   boot.loader.grub.device = "/dev/disk/by-id/ata-SATA_SSD_A45E07221AE300053322";
-  # This makes the system use the XanMod Linux kernel,  a set of
-  # patches reducing latency and improving performance. 
-  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 }
