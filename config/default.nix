2008e9b8975ca37c57d61754140310ab76d32746
quentin@aristote.fr <quentin@aristote.fr>
2024-09-21 17:17:08 +0200

    minimal profile does not disable xlibs anymore

--- config/default.nix
+++ config/default.nix
@@ -9,10 +9,6 @@
     ./users.nix
   ];
 
-  # needed so that the server doesn't rebuild big packages
-  # originally enabled in modulesPath + profiles/minimal.nix
-  environment.noXlibs = false;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
