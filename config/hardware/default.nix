4a5a9700dd5f60ce95fd53a24e99362ce5be6049
quentin@aristote.fr <quentin@aristote.fr>
2025-03-22 21:47:10 +0100

    hardware: use generic config for crypted disk

--- config/hardware/default.nix
+++ config/hardware/default.nix
@@ -1,6 +1,8 @@
-{ nixos-hardware, modulesPath, ... }:
-
 {
+  nixos-hardware,
+  modulesPath,
+  ...
+}: {
   imports = [
     (modulesPath + "/profiles/headless.nix")
     ./hardware-configuration.nix
@@ -12,9 +14,10 @@
   personal.hardware = {
     usb.enable = true;
     firmwareNonFree.enable = true;
+    disks.crypted = "/dev/disk/by-uuid/47e77d74-1aad-4d99-9aa7-568d8524b305";
   };
 
-  swapDevices = [{ device = "/swap"; }];
+  swapDevices = [{device = "/swap";}];
 
   # The CPU frequency should stay at the minimum until the router has
   # some load to compute.
@@ -24,7 +27,7 @@
   # The service irqbalance is useful as it assigns certain IRQ calls
   # to specific CPUs instead of letting the first CPU core to handle
   # everything. This is supposed to increase performance by hitting
-  # CPU cache more often. 
+  # CPU cache more often.
   services.irqbalance.enable = true;
 
   # Re-enable the serial console, disabled by the headless profile
