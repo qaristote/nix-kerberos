3b60d975ef35b8efa0361e214395fff69a9fc423
quentin@aristote.fr <quentin@aristote.fr>
2025-11-30 21:59:14 +0100

    nixos 25.05 -> 25.11
    Flake lock file updates:
    
    • Updated input 'my-nixpkgs':
        'github:qaristote/my-nixpkgs/230bc2319c9e068638ca2191759617b9a331f357' (2025-08-30)
      → 'github:qaristote/my-nixpkgs/e6d25dc678fa289ba488907e3cd49f2f8c330680' (2025-11-30)
    • Updated input 'my-nixpkgs/flake-parts':
        'github:hercules-ci/flake-parts/77826244401ea9de6e3bac47c2db46005e1f30b5' (2025-07-01)
      → 'github:hercules-ci/flake-parts/52a2caecc898d0b46b2b905f058ccc5081f842da' (2025-11-12)
    • Updated input 'my-nixpkgs/flake-parts/nixpkgs-lib':
        'github:nix-community/nixpkgs.lib/14a40a1d7fb9afa4739275ac642ed7301a9ba1ab' (2025-06-29)
      → 'github:nix-community/nixpkgs.lib/719359f4562934ae99f5443f20aa06c2ffff91fc' (2025-10-29)
    • Updated input 'my-nixpkgs/nixpkgs':
        'github:NixOS/nixpkgs/6b4955211758ba47fac850c040a27f23b9b4008f' (2025-07-19)
      → 'github:NixOS/nixpkgs/6f374686605df381de8541c072038472a5ea2e2d' (2025-11-18)
    • Updated input 'my-nixpkgs/nur':
        'github:nix-community/NUR/f333a77a915b0897be437566babbcabe4ffe20fb' (2025-07-21)
      → 'github:nix-community/NUR/05225766ee3843d0720554ab1c930606092c24c5' (2025-11-19)
    • Updated input 'my-nixpkgs/nur/nixpkgs':
        'github:nixos/nixpkgs/c87b95e25065c028d31a94f06a62927d18763fdf' (2025-07-19)
      → 'github:nixos/nixpkgs/89c2b2330e733d6cdb5eae7b899326930c2c0648' (2025-11-17)
    • Updated input 'nixos-hardware':
        'github:NixOS/nixos-hardware/a65b650d6981e23edd1afa1f01eb942f19cdcbb7' (2025-08-26)
      → 'github:NixOS/nixos-hardware/9154f4569b6cdfd3c595851a6ba51bfaa472d9f3' (2025-11-29)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/41d292bfc37309790f70f4c120b79280ce40af16' (2025-08-29)
      → 'github:NixOS/nixpkgs/d542db745310b6929708d9abea513f3ff19b1341' (2025-11-30)

--- flake.nix
+++ flake.nix
@@ -1,29 +1,38 @@
 {
   inputs = {
     my-nixpkgs.url = "github:qaristote/my-nixpkgs";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
-  outputs = {
-    nixpkgs,
-    my-nixpkgs,
-    nixos-hardware,
-    ...
-  }: {
-    nixosConfigurations = let
-      system = "x86_64-linux";
-      commonModules = [
-        my-nixpkgs.nixosModules.personal
-        ({...}: {nixpkgs.overlays = [my-nixpkgs.overlays.personal];})
-      ];
-    in {
-      kerberos = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = commonModules ++ [./config];
-        specialArgs = {
-          inherit nixos-hardware;
+  outputs =
+    {
+      nixpkgs,
+      my-nixpkgs,
+      nixos-hardware,
+      ...
+    }:
+    {
+      nixosConfigurations =
+        let
+          system = "x86_64-linux";
+          commonModules = [
+            my-nixpkgs.nixosModules.personal
+            (
+              { ... }:
+              {
+                nixpkgs.overlays = [ my-nixpkgs.overlays.personal ];
+              }
+            )
+          ];
+        in
+        {
+          kerberos = nixpkgs.lib.nixosSystem {
+            inherit system;
+            modules = commonModules ++ [ ./config ];
+            specialArgs = {
+              inherit nixos-hardware;
+            };
+          };
         };
-      };
     };
-  };
 }
