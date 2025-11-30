c7cda441b7239390cd3f787755553e20fecac8d4
quentin@aristote.fr <quentin@aristote.fr>
2025-11-30 22:56:38 +0100

    boot: use stable kernel

--- config/boot.nix
+++ config/boot.nix
@@ -1,7 +1,7 @@
-{pkgs, ...}: {
+{ ... }:
+{
   personal.boot = {
     grub.enable = true;
   };
   boot.loader.grub.device = "/dev/disk/by-id/ata-SATA_SSD_A45E07221AE300053322";
-  boot.kernelPackages = pkgs.linuxPackages_latest;
 }
